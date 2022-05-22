e20032ad1e4e26265bff344b40662e8619d50566
Renner Nunes <rennercorrea@gmail.com>
2022-05-22 17:53:21 -0300

    TDD com RSpec

---
+++ rspec-unitarios/spec/avengers_spec.rb
@@ -0,0 +1,23 @@
+class AvengersHeadQuarter
+    attr_accessor :list
+
+    def initialize
+        self.list = []
+    end
+
+    def put(avenger)
+        self.list.push(avenger)
+    end
+end
+
+#TDD (Desenvolvimento guiado por testes)
+describe AvengersHeadQuarter do
+    
+    it 'deve adicionar um vingador' do
+        hq = AvengersHeadQuarter.new
+
+        hq.put('Spiderman')
+        expect(hq.list).to eql ['Spiderman']    
+    end
+
+end
