7ac4c9bbef838fb9a983d3ad08c1e7a8a39b94c0
Renner Nunes <rennercorrea@gmail.com>
2022-05-22 18:20:56 -0300

    TDD com RSpec

--- rspec-unitarios/spec/avengers_spec.rb
+++ rspec-unitarios/spec/avengers_spec.rb
@@ -20,4 +20,41 @@ describe AvengersHeadQuarter do
         expect(hq.list).to eql ['Spiderman']    
     end
 
+    it 'deve adicionar uma lista de vingadores' do
+        hq = AvengersHeadQuarter.new
+
+        # Comment Code Block Ctrl+K+C/Ctrl+K+U
+        hq.put('Thor')
+        hq.put('Hulk')
+        hq.put('Spiderman')
+
+        res = hq.list.size > 0
+
+        expect(hq.list).to include 'Hulk'
+        expect(hq.list.size).to be > 0
+        expect(res).to be true
+    end
+
+    it 'Ironman deve ser o último da lista de vingadores' do
+        hq = AvengersHeadQuarter.new
+
+        # Comment Code Block Ctrl+K+C/Ctrl+K+U
+        hq.put('Thor')
+        hq.put('Hulk')
+        hq.put('Spiderman')
+        hq.put('Ironman')
+
+        expect(hq.list).to end_with('Ironman')
+       
+    end
+
+    it 'Deve conter o sobrenome' do
+        
+        avenger = 'Peter Parker'
+
+        expect(avenger).to match(/Parker/)
+        expect(avenger).not_to match(/Renner/)
+       
+    end
+
 end
